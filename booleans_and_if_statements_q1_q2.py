52982f90697f9b9b602ee148395743f62fc2b242
peidcud <51078365+peidcud@users.noreply.github.com>
2020-06-27 12:22:41 +0800

    Updated Q2 & Q3

--- booleans_and_if_statements_q1_q2.py
+++ booleans_and_if_statements_q1_q2.py
@@ -5,14 +5,14 @@ moths_in_house = input(moths_prompt)
 mitch_prompt = f"and is Mitch home? Please answer True or False "
 mitch_is_home = input(mitch_prompt)
 
-if moths_in_house == "True" and mitch_is_home == "True":
-     print("Hoooman! Help me get the moths!")
+if moths_in_house == "True":
+     if mitch_is_home == "True":
+          print("Hoooman! Help me get the moths!")
+     else:
+          print("Meooooooooooooow! Hissssss!")
 
-if moths_in_house == "False" and mitch_is_home == "False":
-     print("No threats detected.")
-
-if moths_in_house == "True" and mitch_is_home == "False":
-     print("Meooooooooooooow! Hissssss!")
-
-if moths_in_house == "False" and mitch_is_home == "True":
-     print("Climb on Mitch.")
+if moths_in_house == "False":
+     if mitch_is_home == "True":
+          print("Climb on Mitch.")
+     else:
+          print("No threats detected.")
--- booleans_and_if_statements_q3.py
+++ booleans_and_if_statements_q3.py
@@ -5,15 +5,7 @@ light_colour = input(light_prompt)
 car_prompt = f"and is there a car nearby? Please answer True or False "
 car_detected = input(car_prompt)
 
-if light_colour == "Red" and car_detected == "False":
-     print("Do nothing.")
 if light_colour == "Red" and car_detected == "True":
      print("Flash!")
-if light_colour == "Green" and car_detected == "False":
-     print("Do nothing.")
-if light_colour == "Green" and car_detected == "True":
-     print("Do nothing.")
-if light_colour == "Amber" and car_detected == "False":
-     print("Do nothing.")
-if light_colour == "Amber" and car_detected == "True":
+else:
      print("Do nothing.")
