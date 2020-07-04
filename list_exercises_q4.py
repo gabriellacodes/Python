91bf33ecbc53f5dcd5e1f853e5b1537d91153995
peidcud <51078365+peidcud@users.noreply.github.com>
2020-07-04 19:11:47 +0800

    list question 4 completed

--- list_exercises_q4.py
+++ list_exercises_q4.py
@@ -1,14 +1,6 @@
-sentence_list = []
+sentence = input(f"For my next trick I will make your sentence completely unreadable. Tell me a sentence you want me to just destroy: ")
 
-sentence_prompt = f"For my next trick I will make your sentence completely unreadable. Tell me a sentence you want me to just destroy: "
-sentence_list = input(sentence_prompt)
+word_list = sentence.split()
+print(len(word_list), word_list)
 
-# print(f"Here: ",sentence_list)
-
-word_list = sentence_list.split()
-print(len(word_list))
-
-for x in word_list:
-    char_list = x.list()
-
-print(word_list)
+print(len(sentence), list(sentence))
